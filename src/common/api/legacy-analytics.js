22e2ab4a356d1f88065c0503630d57d472fe4f49
Anthony Liddle <anthonyliddle@gmail.com>
2021-01-14 08:23:01 -0800

    fix(qa): adding legacy analytics logging for qa verification

--- src/common/api/legacy-analytics.js
+++ src/common/api/legacy-analytics.js
@@ -2,5 +2,6 @@ import { track as trackRequest } from '@pocket/web-utilities/legacy-analytics'
 import { CONSUMER_KEY } from 'common/constants'
 
 export const legacyAnalyticsTrack = (params) => {
+  console.log({ ...params })
   return trackRequest(CONSUMER_KEY, params)
 }
