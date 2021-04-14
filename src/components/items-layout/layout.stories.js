c308db028deecc766d67a979716ec873f5748f8f
Joel Kelly <joel@getpocket.com>
2021-04-13 21:50:26 -0700

    refactor(card layouts): add stories for lockups

--- src/components/items-layout/layout.stories.js
+++ src/components/items-layout/layout.stories.js
@@ -4,38 +4,29 @@ import { deriveDiscoverItems } from 'connectors/items-by-id/discover/items.deriv
 import myListResponse from 'mock/my-list.json'
 import discoverResponse from 'mock/discover.json'
 import { css } from 'linaria'
+import { cardsGrid } from './base'
 
 const discoverItems = deriveDiscoverItems(discoverResponse.feed).map((item) => {
   item.story_name = `Discover - ${item.title}`
   return item
 })
 
-const myListItems = deriveMyListItems(Object.values(myListResponse.list)).map(
-  (item) => {
-    item.story_name = `My List - ${item.title}`
-    return item
-  }
-)
+const myListItems = deriveMyListItems(Object.values(myListResponse.list)).map((item) => {
+  item.story_name = `My List - ${item.title}`
+  return item
+})
 
 const itemsToDisplay = [...myListItems, ...discoverItems]
 
 const grid = css`
   max-width: 936px;
-  display: grid;
-  align-items: start;
-  grid-column-gap: var(--spacing150);
-  grid-template-columns: repeat(12, 1fr);
+  ${cardsGrid};
 `
 
 export default {
   title: 'Card/Card Layouts',
   component: CardComponent,
   argTypes: {
-    itemType: {
-      control: {
-        type: 'inline-radio'
-      }
-    },
     cardShape: {
       control: {
         type: 'inline-radio'
@@ -88,38 +79,31 @@ GridOfCards.args = {
 }
 
 export const MixedLayout = (args) => {
+  const hero = {
+    cardShape: 'block',
+    className: 'hero-block',
+    showMedia: true
+  }
+  const subset = {
+    cardShape: 'detail',
+    className: 'subset',
+    showExcerpt: true,
+    showMedia: true,
+    itemType: 'discover'
+  }
+
+  const layout = {
+    0: hero,
+    1: subset,
+    2: subset,
+    3: subset
+  }
+
   return (
     <div className={grid}>
       {itemsToDisplay.map((item, index) => {
-        const hero = {
-          cardShape: 'block',
-          className: 'hero-block',
-          showMedia: true
-        }
-        const subset = {
-          cardShape: 'detail',
-          className: 'subset',
-          showExcerpt: true,
-          showMedia: true,
-          itemType: 'discover'
-        }
-
-        const layout = {
-          0: hero,
-          1: subset,
-          2: subset,
-          3: subset
-        }
-
         const argsToPass = { ...args, ...layout[index] }
-        return (
-          <CardComponent
-            item={item}
-            position={0}
-            actions={{}}
-            {...argsToPass}
-          />
-        )
+        return <CardComponent item={item} position={0} actions={{}} {...argsToPass} />
       })}
     </div>
   )
@@ -130,4 +114,98 @@ MixedLayout.args = {
   showMedia: true,
   itemType: 'discover',
   cardShape: 'block'
+}
+
+export const CenterLockup = (args) => {
+  const hero = {
+    className: 'hero-center',
+    cardShape: 'grid',
+    itemType: 'discover',
+    showExcerpt: true
+  }
+  const base = {
+    className: 'lockup',
+    cardShape: 'grid',
+    itemType: 'discover'
+  }
+
+  const layout = {
+    0: hero,
+    1: base,
+    2: base,
+    3: base,
+    4: base
+  }
+
+  return (
+    <div className={grid}>
+      {itemsToDisplay.slice(0, 5).map((item, index) => {
+        const argsToPass = { ...args, ...layout[index] }
+        return <CardComponent item={item} position={0} actions={{}} {...argsToPass} />
+      })}
+    </div>
+  )
+}
+
+export const LeftLockup = (args) => {
+  const hero = {
+    className: 'hero-left',
+    cardShape: 'grid',
+    itemType: 'discover',
+    showExcerpt: true
+  }
+
+  const base = {
+    className: 'lockup',
+    cardShape: 'grid',
+    itemType: 'discover'
+  }
+
+  const layout = {
+    0: hero,
+    1: base,
+    2: base,
+    3: base,
+    4: base
+  }
+
+  return (
+    <div className={grid}>
+      {itemsToDisplay.slice(0, 5).map((item, index) => {
+        const argsToPass = { ...args, ...layout[index] }
+        return <CardComponent item={item} position={0} actions={{}} {...argsToPass} />
+      })}
+    </div>
+  )
+}
+
+export const RightLockup = (args) => {
+  const hero = {
+    className: 'hero-right',
+    cardShape: 'grid',
+    itemType: 'discover',
+    showExcerpt: true
+  }
+  const base = {
+    className: 'lockup',
+    cardShape: 'grid',
+    itemType: 'discover'
+  }
+
+  const layout = {
+    0: hero,
+    1: base,
+    2: base,
+    3: base,
+    4: base
+  }
+
+  return (
+    <div className={grid}>
+      {itemsToDisplay.slice(0, 5).map((item, index) => {
+        const argsToPass = { ...args, ...layout[index] }
+        return <CardComponent item={item} position={0} actions={{}} {...argsToPass} />
+      })}
+    </div>
+  )
 }
