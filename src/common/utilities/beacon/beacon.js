6f33f2de70ccf2b97e841f0ef6ad2bb75540e590
Aly Fluckey <afluckey@mozilla.com>
2021-07-08 16:35:44 -0500

    chore(cleanup): removing unused beacon functions

--- src/common/utilities/beacon/beacon.js
+++ src/common/utilities/beacon/beacon.js
@@ -1,36 +1,8 @@
 import queryString from 'query-string'
-import { request } from 'common/utilities/request/request'
 
 export const API_URL = process.env.POCKET_API_URL
   ? process.env.POCKET_API_URL
   : 'https://getpocket.com/v3'
-
-/**
- * sendBeacon implements navigator beacon if it's supported in order to send
- * analytics reliably on page changes
- * @param {string} consumerKey Consumer key passed in from the consuming app
- * @param {Object} options Details about the request (path and query params)
- */
-export function sendBeacon(consumerKey, options) {
-  if (!options?.path) {
-    throw new Error('options.path is required by request()')
-  }
-
-  // If we don't support beacon we resort to an async fetch
-  if (!navigator?.beacon) return blockingRequest(consumerKey, options)
-
-  const url = buildBeaconUrl(consumerKey, options)
-  navigator.sendBeacon(url)
-}
-
-/**
- * blockingRequest makes a fetch request synchronously
- * @param {string} consumerKey Consumer key passed in from the consuming app
- * @param {Object} options Details about the request (path and query params)
- */
-const blockingRequest = async function (consumerKey, options) {
-  await request(consumerKey, options)
-}
 
 /**
  * buildBeaconUrl Appends consumer_key to the url to identify app with the API.
