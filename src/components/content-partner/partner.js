ba5299f9121a92b74c0e32b9214e4ddbd028f40c
Aly Fluckey <afluckey@mozilla.com>
2021-07-23 16:09:54 -0500

    fix(partner): update translation keys to be cleaner

--- src/components/content-partner/partner.js
+++ src/components/content-partner/partner.js
@@ -31,8 +31,8 @@ export function Partner({partnerInfo}) {
 
   return (
     <div className={partnerStyles}>
-      { type === 'partner' ? <div>{t('partner-info:partner-copy', 'In partnership with')}</div> : null}
-      { type === 'sponsor' ? <div>{t('partner-info:sponsor-copy', 'Brought to you by')}</div> : null}
+      { type === 'partner' ? <div>{t('partner:partner-copy', 'In partnership with')}</div> : null}
+      { type === 'sponsor' ? <div>{t('partner:sponsor-copy', 'Brought to you by')}</div> : null}
       <a href={partnerInfo.url} target='_blank' rel='noopener noreferrer'>
         <img src={partnerInfo.imageUrl} alt="" />
       </a>
@@ -47,8 +47,8 @@ export function PartnerOverline({partnerType}) {
 
   return (
     <div className={overlineStyles}>
-      { partnerType === 'partner' ? t('partner-card:partner-copy', 'From our partners') : null}
-      { partnerType === 'sponsor' ? t('partner-card:sponsor-copy', 'Sponsored') : null}
+      { partnerType === 'partner' ? t('partner:partner-overline', 'From our partners') : null}
+      { partnerType === 'sponsor' ? t('partner:sponsor-overline', 'Sponsored') : null}
     </div>
   )
 }
