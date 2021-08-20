20b7f3c9275ef3a7328c23c8d49396dceb54eb15
Anthony Liddle <anthonyliddle@gmail.com>
2021-08-20 12:19:36 -0700

    fix(onetrust): call correct function in timeout

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -48,11 +48,11 @@ class ClientDocument extends Document {
               var timeHolder;
 
               function dispatchOneTrust(groups){
-                if(typeof __NEXT_REDUX_WRAPPER_STORE__ !== 'undefined') {
+                if(typeof __NEXT_REDUX_WRAPPER_STORE__ !== 'undefined' && groups) {
                   __NEXT_REDUX_WRAPPER_STORE__.dispatch({type: 'ONE_TRUST_DATA', groups})
                 } else {
                   clearTimeout(timeHolder); // clear for safety
-                  timeHolder = setTimeout(dispatchOneTrust, 50)
+                  timeHolder = setTimeout(OptanonWrapper, 50)
                 }
               }
 
@@ -68,32 +68,32 @@ class ClientDocument extends Document {
           <meta charSet="utf-8" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge, chrome=1" />
           <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no" />
-          
+
           {/* Android  */}
           <meta name="theme-color" media="(prefers-color-scheme: light)" content="white" />
           <meta name="theme-color" media="(prefers-color-scheme: dark)" content="black" />
           <meta name="mobile-web-app-capable" content="yes" />
-          
+
           {/* iOS */}
           <meta name="apple-mobile-web-app-title" content="Pocket" />
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-          
+
           {/* Windows */}
           <meta name="msapplication-TileColor" content="#ef4056" />
           <meta name="msapplication-TileImage" content="/i/apple-touch-icon/Pocket_AppIcon_@144.png" />
-          
+
           {/* Pinned Sites */}
           <meta name="application-name" content="Pocket" />
           <meta name="msapplication-tooltip" content="Pocket" />
           <meta name="msapplication-starturl" content="/" />
-          
+
           {/* UC Mobile Browser */}
           <meta name="full-screen" content="yes" />
           <meta name="browsermode" content="application" />
           <meta name="viewport" content="uc-fitscreen=yes" />
           <meta name="layoutmode" content="fitscreen/standard" />
-          
+
           {/* manifest.json provides metadata used when your web app is added to the homescreen on Android. See https://developers.google.com/web/fundamentals/engage-and-retain/web-app-manifest/ */}
           <link rel="manifest" href="/manifest.json" />
 
