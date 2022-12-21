b492cc4556f3a8b1e36c920af5e3610a28f66a73
Joel Kelly <joel@getpocket.com>
2022-12-21 11:00:46 -0800

    fix(home): use common saves state

--- src/containers/home/home.state.js
+++ src/containers/home/home.state.js
@@ -4,12 +4,6 @@ import { saveItem } from 'common/api/_legacy/saveItem'
 import { arrayToObject } from 'common/utilities/object-array/object-array'
 import { deriveListItem } from 'common/api/derivers/item'
 import { removeItemByUrl } from 'common/api/_legacy/removeItem'
-import { STARTER_ARTICLES } from 'common/constants'
-import { getSaves } from 'common/api/saves'
-
-import { HOME_RECENT_SAVES_REQUEST } from 'actions'
-import { HOME_RECENT_SAVES_SUCCESS } from 'actions'
-import { HOME_RECENT_SAVES_FAILURE } from 'actions'
 
 import { HOME_CONTENT_REQUEST } from 'actions'
 import { HOME_CONTENT_SUCCESS } from 'actions'
@@ -27,7 +21,6 @@ import { SET_TOPIC_SUCCESS } from 'actions'
 export const getHomeContent = (id) => ({ type: HOME_CONTENT_REQUEST, id })
 export const saveHomeItem = (id, url) => ({ type: HOME_SAVE_REQUEST, id, url })
 export const unSaveHomeItem = (id, url) => ({ type: HOME_UNSAVE_REQUEST, id, url })
-export const getRecentSaves = () => ({ type: HOME_RECENT_SAVES_REQUEST })
 
 /** REDUCERS
  --------------------------------------------------------------- */
@@ -44,29 +37,6 @@ export const homeReducers = (state = initialState, action) => {
       return { ...state, itemsById, slatesById, slates: slateArray }
     }
 
-    case HOME_SAVE_SUCCESS: {
-      const { corpusId, id } = action
-      const item = state.itemsById[corpusId] || {}
-      const recentSaves = Array.from(new Set([id, ...state.recentSaves]))
-
-      return {
-        ...state,
-        recentSaves,
-        itemsById: { ...state.itemsById, [corpusId]: { ...item, savedId: id, saveStatus: 'saved' } }
-      }
-    }
-
-    case HOME_UNSAVE_SUCCESS: {
-      const { corpusId } = action
-      const item = state.itemsById[corpusId] || {}
-      const recentSaves = state.recentSaves.filter((id) => id !== item.savedId)
-      return {
-        ...state,
-        recentSaves,
-        itemsById: { ...state.itemsById, [corpusId]: { ...item, saveStatus: 'unsaved' } }
-      }
-    }
-
     case HOME_SAVE_REQUEST:
     case HOME_UNSAVE_REQUEST: {
       const { id } = action
@@ -75,12 +45,6 @@ export const homeReducers = (state = initialState, action) => {
         ...state,
         itemsById: { ...state.itemsById, [id]: { ...item, saveStatus: 'saving' } }
       }
-    }
-
-    case HOME_RECENT_SAVES_SUCCESS: {
-      const { items } = action
-      const recentSaves = new Set([...items])
-      return { ...state, recentSaves: Array.from(recentSaves) }
     }
 
     default: {
@@ -95,7 +59,6 @@ export const homeSagas = [
   takeEvery(HOME_CONTENT_REQUEST, homeContentRequest),
   takeEvery(SET_TOPIC_SUCCESS, homeContentRequest),
   takeEvery(HOME_SAVE_REQUEST, homeContentSaveRequest),
-  takeEvery(HOME_RECENT_SAVES_REQUEST, recentDataRequest),
   takeEvery(HOME_UNSAVE_REQUEST, homeContentUnSaveRequest)
 ]
 
@@ -145,49 +108,3 @@ function* homeContentUnSaveRequest({ id, url }) {
   }
 }
 
-function* recentDataRequest() {
-  try {
-    const { itemsById, error } = yield fetchSavesData({
-      count: 3,
-      offset: 0,
-      state: 'unread',
-      sort: 'newest'
-    })
-    if (error) yield put({ type: HOME_RECENT_SAVES_FAILURE, error })
-
-    // Remove default item for Home Experiment
-    const items = Object.values(itemsById)
-      .filter((item) => !STARTER_ARTICLES.includes(item.itemId))
-      .sort((a, b) => b._createdAt - a._createdAt)
-      .map((item) => item.itemId)
-
-    yield put({ type: HOME_RECENT_SAVES_SUCCESS, items, itemsById }) // prettier-ignore
-  } catch (error) {
-    console.warn(error)
-    yield put({ type: HOME_RECENT_SAVES_FAILURE, error })
-  }
-}
-
-/** ASYNC Functions
- --------------------------------------------------------------- */
-/**
- * fetchSavesData
- * Make and async request for a Pocket v3 feed and return best data
- * @return items {array} An array of derived items
- */
-export async function fetchSavesData(params) {
-  try {
-    const response = await getSaves(params)
-    if (!response.list) return { error: 'No Items Returned' }
-
-    const total = response.total
-
-    const derivedItems = Object.values(response.list).map((item) => deriveListItem(item, true))
-    const itemsById = arrayToObject(derivedItems, 'itemId')
-
-    return { itemsById, total }
-  } catch (error) {
-    //TODO: adjust this once error reporting strategy is defined.
-    console.warn('discover.state', error)
-  }
-}
