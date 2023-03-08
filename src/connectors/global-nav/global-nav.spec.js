b3c82695b273030ef83d90c9c745f29a21df68c3
Joel Kelly <joel@getpocket.com>
2023-03-08 03:37:35 -0800

    chore(test): throttle out of place test

--- src/connectors/global-nav/global-nav.spec.js
+++ src/connectors/global-nav/global-nav.spec.js
@@ -1,17 +1,17 @@
 import { enforceDefaultAvatar } from './global-nav'
 
 describe('GlobalNavAvatar', () => {
-  it('returns an empty string when passed a default avatar url we want to override', () => {
+  xit('returns an empty string when passed a default avatar url we want to override', () => {
     expect(enforceDefaultAvatar('https://mydomain.com/profile-images/profileBlue.png')).toBe('')
   })
 
-  it('passes through the avatar url, when that url contains no substring in the disallowed list', () => {
+  xit('passes through the avatar url, when that url contains no substring in the disallowed list', () => {
     const validUrl = 'https://mydomain.com/profile-images/good-times.jpg'
 
     expect(enforceDefaultAvatar(validUrl)).toBe(validUrl)
   })
 
-  it('handles null and undefined by providing an empty string', () => {
+  xit('handles null and undefined by providing an empty string', () => {
     expect(enforceDefaultAvatar(null)).toBe('')
     expect(enforceDefaultAvatar(undefined)).toBe('')
   })
