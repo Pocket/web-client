66e1df3a631f565e4c8fe361c3f3129559192d8d
Anthony Liddle <anthonyliddle@gmail.com>
2023-05-03 13:50:27 -0700

    fix(reorder): cleanup reorder lists header styles

--- src/components/headers/lists-header.js
+++ src/components/headers/lists-header.js
@@ -75,10 +75,6 @@ const listHeaderStyles = css`
       .create-reorder {
         width: 100%;
 
-        .save {
-          flex-grow: 2;
-        }
-
         .cancel {
           flex-grow: 1;
         }
