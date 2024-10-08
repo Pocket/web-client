f7364574305a8705de7e7495dd4a15fcd3cb6922
Joel <joel.kelly@me.com>
2024-10-07 21:56:15 -0700

    fix(deriver): set up unique derivers

--- clients/web/src/common/api/derivers/preview.ts
+++ clients/web/src/common/api/derivers/preview.ts
@@ -1,6 +1,11 @@
-import type { CorpusSearchEdge } from '@common/types/pocket'
+import {
+  type CorpusSearchEdge,
+  type SavedItemEdge,
+  type PendingItem,
+  PendingItemStatus
+} from '@common/types/pocket'
 
-export function deriveCorpusPreview(edge: CorpusSearchEdge) {
+export function deriveCorpusSearch(edge: CorpusSearchEdge) {
   const { node, cursor } = edge
   const { item, searchHighlights } = node
   const { preview } = item ?? {}
@@ -17,4 +22,29 @@ export function deriveCorpusPreview(edge: CorpusSearchEdge) {
     cursor,
     utmId: 'pocket_search'
   }
+}
+
+export function deriveSavedItem(edge: SavedItemEdge) {
+  const { node, cursor } = edge
+  const item = node?.item
+  if (isPendingItem(item)) return {}
+
+  const preview = item?.preview
+  const cached = preview?.image?.cachedImages ?? false
+  const thumbnail = cached ? cached[0]?.url : ''
+
+  return {
+    item: {
+      ...preview,
+      publisher: preview?.domain?.name,
+      thumbnail
+    },
+    cursor,
+    utmId: 'pocket_saves'
+  }
+}
+
+// Function to check if the object is a PendingItem
+function isPendingItem(item: unknown): item is PendingItem {
+  return (item as PendingItem).status === PendingItemStatus.Unresolved
 }
--- clients/web/src/common/api/queries/get-corpus-search.js
+++ clients/web/src/common/api/queries/get-corpus-search.js
@@ -1,6 +1,6 @@
 import { gql } from 'common/utilities/gql/gql'
 import { requestGQL } from 'common/utilities/request/request'
-import { deriveCorpusPreview } from 'common/api/derivers/preview'
+import { deriveCorpusSearch } from 'common/api/derivers/preview'
 import { FRAGMENT_ITEM_PREVIEW } from 'common/api/fragments/fragment.preview'
 
 const getSearchQuery = gql`
@@ -67,7 +67,7 @@ function getItemsByIdFromEdges(edges) {
   return edges.reduce((previous, current) => {
     if (!current?.node?.item?.preview?.id) return previous
 
-    const { item, searchHighlights } = deriveCorpusPreview(current)
+    const { item, searchHighlights } = deriveCorpusSearch(current)
     return {
       ...previous,
       [item.id]: {
