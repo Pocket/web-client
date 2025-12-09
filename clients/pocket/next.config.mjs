98074a36508d9b3738345df29d45c805e7ba4c5b
Joel <joel.kelly@me.com>
2025-12-09 11:00:58 -0800

    chore(pocket): remove rsc version (defunct now)

--- clients/pocket/next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  transpilePackages: ['@ui/styles', '@common/localization'],
-  output: 'standalone',
-  bundlePagesRouterDependencies: true,
-  assetPrefix: process.env.ASSET_PREFIX
-}
-
-export default nextConfig
